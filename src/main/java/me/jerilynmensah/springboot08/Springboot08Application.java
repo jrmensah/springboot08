1aa035a96a74418fd0a53255dd22d50cd57e93cf
jrmensah <jmensah4@student.umuc.edu>
2017-08-21 16:35:35 -0400

    First commit- Using databases with spring boot

---
+++ src/main/java/me/jerilynmensah/springboot08/Springboot08Application.java
@@ -0,0 +1,12 @@
+package me.jerilynmensah.springboot08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot08Application.class, args);
+	}
+}
